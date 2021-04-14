5670fdfd7662014308a8d5f090857989ce15be45
montaser55 <majidmontaser@gmail.com>
2021-04-14 22:35:35 +0600

    Arrays

--- Arrays/RotateMatrix.java
+++ Arrays/RotateMatrix.java
@@ -4,7 +4,7 @@ class Solution {
 	//Space: O(1)
 	public void rotateEquation(int[][] matrix) {
         int n=matrix.length;
-        for(int i=0;i<n/2;i++){
+        for(int i=0;i<(n+1)/2;i++){
             for(int j=0;j<n/2;j++){
                 int temp=matrix[n-1-j][i];
                 matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
